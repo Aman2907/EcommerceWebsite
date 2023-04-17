07a14cb24dcacb7243b044b87184b7d928506abe
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2023-04-17 09:46:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home';
+import { Menu } from './pages/Menu'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+       <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+export default App;
---
+++ src/Helpers/MenuList.js
@@ -0,0 +1,42 @@
+
+
+import Pepperoni from "../photo/pepperoni.jpg";
+import Margherita from "../photo/margherita.jpg";
+import PedroTechSpecial from "../photo/pedrotechspecial.jpg";
+import Vegan from "../photo/vegan.jpg";
+import Pineapple from "../photo/pineapple.jpg";
+import Expensive from "../photo/expensive.jpg";
+
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 15.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
